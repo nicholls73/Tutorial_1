b601063d11152d260a80577165c1c9eefc9e49f7
James Nicholls <jamnich73@gmail.com>
2022-08-09 00:25:46 +0800

    9/8/2022 - 12:25 AM

---
+++ Server/ServerProgram.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+
+namespace Server {
+    internal class ServerProgram {
+        static void Main(string[] args) {
+            Console.WriteLine("Welcome to my server");
+            ServiceHost host;
+            NetTcpBinding tcp = new NetTcpBinding();
+
+            host = new ServiceHost(typeof(DataServer));
+            host.AddServiceEndpoint(typeof(DataServerInterface), tcp, "net.tcp://0.0.0.0:8100/DataService");
+
+            host.Open();
+            Console.WriteLine("System Online");
+            Console.ReadLine();
+            host.Close();
+        }
+    }
+}
